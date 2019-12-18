dfa0dd176098aca0adbb44c20bdf3104c12264a6
Vanroy Julien <julienvanroy@yahoo.fr>
2019-12-18 12:46:12 +0100

    feat: notice

--- src/app/components/Popin/Notice.tsx
+++ src/app/components/Popin/Notice.tsx
@@ -1,15 +1,26 @@
-import React, { FunctionComponent, CSSProperties } from 'react'
+import React, {FunctionComponent, CSSProperties, useState, useEffect} from 'react'
+import {useSpring, animated} from "react-spring";
 
 type NoticeProps = {
     menu?: any
-    className?: string
     style?: CSSProperties
+    active: boolean
 }
 
-const Notice: FunctionComponent<NoticeProps> = ({ menu = [], className = '', style = {} }) => (
-    <nav className={`menu${className}`} style={style} >
-        {menu.lenght > 0 || menu ? <ul>{menu.map((item, index) => <li key={index} onClick={item.click}>{item.name}</li>)}</ul> : null }
-    </nav>
-)
+const Notice: FunctionComponent<NoticeProps> = ({menu = [], style = {}, active= false}) => {
+
+    const props = useSpring({
+        opacity: active ? 1 : 0
+    })
+
+    return (
+        <animated.div className={`notice${active ? ' active' : ''}`} style={{...style, ...props}}>
+            <div className="notice-content">
+                Notice
+            </div>
+            <div className="scroll">Scroll vers le bas</div>
+        </animated.div>
+    )
+}
 
 export default Notice
