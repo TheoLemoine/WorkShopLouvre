8ffc31d95e0ba47276e0b3b6c99a4d714f04aeb8
Vanroy Julien <julienvanroy@yahoo.fr>
2020-01-18 00:39:21 +0100

    fix: log

--- src/app/serviceWorker.ts
+++ src/app/serviceWorker.ts
@@ -31,10 +31,6 @@ export function register(config?: Config) {
     const publicUrl = new URL(
       window.location.href
     );
-    console.log(new URL(
-        window.location.href
-    ))
-      console.log(publicUrl)
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
